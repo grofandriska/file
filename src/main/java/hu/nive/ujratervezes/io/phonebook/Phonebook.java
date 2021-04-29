3e4c878f76428a736f39a9a6e9b34b1b66992ef7
Gróf András <andrasgrof93@gmail.com>
2021-04-29 11:43:30 +0200

    file ok

--- src/main/java/hu/nive/ujratervezes/io/phonebook/Phonebook.java
+++ src/main/java/hu/nive/ujratervezes/io/phonebook/Phonebook.java
@@ -1,3 +1,31 @@
 package hu.nive.ujratervezes.io.phonebook;
 
-public class Phonebook {}
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+public class Phonebook {
+    public void exportPhonebook(Map<String, String> contacts, String output) {
+        try {
+            if (contacts == null) {
+                throw new IllegalArgumentException();
+            }
+            File file = new File(output);
+
+            BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(file));
+            for (Map.Entry<String, String> entry : contacts.entrySet()) {
+                bufferedWriter.write(entry.getKey() + ": " + entry.getValue());
+                bufferedWriter.newLine();
+            }
+            bufferedWriter.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (NullPointerException e) {
+            throw new IllegalArgumentException();
+        }
+    }
+}
